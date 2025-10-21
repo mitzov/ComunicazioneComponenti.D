b371454fe63d643d0cf1436c55a5e6d74a6bc15a
mitzov <159139102+mitzov@users.noreply.github.com>
2025-10-21 06:51:23 +0000

    fine progetto controllo errori

--- componenti/src/app/app.ts
+++ componenti/src/app/app.ts
@@ -1,14 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Componente1 } from './componente1/componente1';
+import { Post } from './post/post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Componente1],
+  imports: [RouterOutlet,Componente1,Post],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('componenti');
   messaggio = 'Ciao dal componente padre!';
+  posts = [
+    {
+    titolo: 'titolo1',
+    testo: 'esempio di testo1'
+    },
+    {
+    titolo: 'titolo1',
+    testo: 'esempio di testo2'
+    },
+    {
+    titolo: 'titolo3',
+    testo: 'esempio di testo3'
+    }
+    ];
 }
